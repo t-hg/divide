4be4e7976fcb398d6783645c4a63392b3c8e25cf
Tobias Hort-Giess <tobias.hortgiess@gmail.com>
2024-02-21 06:57:12 +0000

    divide room, results in the bigger room

--- model/room.go
+++ model/room.go
@@ -1,6 +1,83 @@
 package model
 
+import "math/rand"
+
 type Room struct {
 	PosX, PosY    int32
 	Width, Height int32
+}
+
+const (
+	DirHorizontal = iota
+	DirVertical
+)
+
+func (room *Room) Divide(posX, posY int32, dir int) {
+	if posX < room.PosX || posX > room.PosX+room.Width {
+		return
+	}
+	if posY < room.PosY || posY > room.PosY+room.Height {
+		return
+	}
+	switch dir {
+	case DirHorizontal:
+		upperRoom := Room{
+			PosX:   room.PosX,
+			PosY:   room.PosY,
+			Width:  room.Width,
+			Height: posY - room.PosY,
+		}
+		lowerRoom := Room{
+			PosX:   room.PosX,
+			PosY:   posY,
+			Width:  room.Width,
+			Height: room.PosY + room.Height - posY,
+		}
+        biggerRoom := biggerRoom(upperRoom, lowerRoom)
+        room.PosX = biggerRoom.PosX
+        room.PosY = biggerRoom.PosY
+        room.Height = biggerRoom.Height
+	case DirVertical:
+        leftRoom := Room {
+            PosX: room.PosX,
+            PosY: room.PosY,
+            Width: posX - room.PosX,
+            Height: room.Height, 
+        }
+        rightRoom := Room {
+            PosX: posX,
+            PosY: room.PosY,
+            Width: room.PosX + room.Width - posX,
+            Height: room.Height, 
+        }
+        biggerRoom := biggerRoom(leftRoom, rightRoom)
+        room.PosX = biggerRoom.PosX
+        room.PosY = biggerRoom.PosY
+        room.Width = biggerRoom.Width
+	default:
+		return
+	}
+}
+
+func biggerRoom(roomA, roomB Room) Room {
+	if area(roomA) == area(roomB) {
+		return randomRoom(roomA, roomB)
+	} else if area(roomA) > area(roomB) {
+		return roomA
+	} else {
+		return roomB
+	}
+}
+
+func randomRoom(roomA, roomB Room) Room {
+	n := rand.Intn(2)
+	if n == 0 {
+		return roomA
+	} else {
+		return roomB
+	}
+}
+
+func area(room Room) int32 {
+	return room.Width * room.Height
 }
---
+++ model/room_test.go
@@ -0,0 +1,109 @@
+package model
+
+import "testing"
+
+func TestDivide(t *testing.T) {
+	room := Room{}
+
+	resetRoom := func() {
+		room = Room{
+			PosX:   50,
+			PosY:   100,
+			Width:  350,
+			Height: 400,
+		}
+	}
+
+	resetRoom()
+
+	tests := []struct {
+		PosX, PosY   int32
+		Dir          int
+		ExpectedRoom Room
+	}{
+		{
+			PosX: 49,
+			PosY: 150,
+			Dir:  DirHorizontal,
+			// x out of bound
+			ExpectedRoom: room,
+		},
+		{
+			PosX: 401,
+			PosY: 150,
+			Dir:  DirHorizontal,
+			// x out of bound
+			ExpectedRoom: room,
+		},
+		{
+			PosX: 100,
+			PosY: 99,
+			Dir:  DirHorizontal,
+			// y out of bound
+			ExpectedRoom: room,
+		},
+		{
+			PosX: 100,
+			PosY: 501,
+			Dir:  DirHorizontal,
+			// y out of bound
+			ExpectedRoom: room,
+		},
+        {
+			PosX: 51,
+			PosY: 200,
+			Dir:  DirHorizontal,
+            // lower room 
+            ExpectedRoom: Room{
+                PosX: 50,
+                PosY: 200,
+                Width: 350,
+                Height: 300,
+            },
+        },
+        {
+			PosX: 51,
+			PosY: 350,
+			Dir:  DirHorizontal,
+            // upper room 
+            ExpectedRoom: Room{
+                PosX: 50,
+                PosY: 100,
+                Width: 350,
+                Height: 250,
+            },
+        },
+        {
+			PosX: 150,
+			PosY: 101,
+			Dir:  DirVertical,
+            // right room 
+            ExpectedRoom: Room{
+                PosX: 150,
+                PosY: 100,
+                Width: 250,
+                Height: 400,
+            },
+        },
+        {
+			PosX: 250,
+			PosY: 101,
+			Dir:  DirVertical,
+            // left room 
+            ExpectedRoom: Room{
+                PosX: 50,
+                PosY: 100,
+                Width: 200,
+                Height: 400,
+            },
+        },
+	}
+
+	for _, test := range tests {
+		room.Divide(test.PosX, test.PosY, test.Dir)
+        if room != test.ExpectedRoom {
+            t.Errorf("Expected %v, got %v", test.ExpectedRoom, room)
+        }
+        resetRoom()
+	}
+}
