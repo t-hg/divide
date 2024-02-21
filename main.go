214bd1fb633e0cd812cb4a517c4573c51033745a
Tobias Hort-Giess <tobias.hortgiess@gmail.com>
2024-02-21 18:32:05 +0000

    mouse click divides

--- main.go
+++ main.go
@@ -12,23 +12,39 @@ const (
 
 func main() {
     room := model.Room{
-        PosX: 300,
-        PosY: 300,
-        Width: 150,
-        Height: 150,
+        PosX: 0,
+        PosY: 0,
+        Width: ScreenWidth,
+        Height: ScreenHeight,
     }
-	balls := model.RandomBalls(room, 25)
+	
+    balls := model.RandomBalls(room, 25)
+
 	rl.InitWindow(ScreenWidth, ScreenHeight, "divide")
 	defer rl.CloseWindow()
+
 	rl.SetTargetFPS(60)
+
 	for !rl.WindowShouldClose() {
 		rl.BeginDrawing()
 		rl.ClearBackground(rl.Black)
         rl.DrawRectangle(room.PosX, room.PosY, room.Width, room.Height, rl.RayWhite)
+
 		for i := range balls {
-			rl.DrawCircle(balls[i].PosX, balls[i].PosY, 4, rl.Black)
+			rl.DrawCircle(balls[i].PosX, balls[i].PosY, 4, rl.Red)
 			balls[i].NextPosition(room)
 		}
+
+        if rl.IsMouseButtonPressed(rl.MouseButtonLeft) {
+            pos := rl.GetMousePosition()
+            room.Divide(int32(pos.X), int32(pos.Y), model.DirHorizontal)
+        }
+
+        if rl.IsMouseButtonPressed(rl.MouseButtonRight) {
+            pos := rl.GetMousePosition()
+            room.Divide(int32(pos.X), int32(pos.Y), model.DirVertical)
+        }
+
 		rl.EndDrawing()
 	}
 }
--- model/ball.go
+++ model/ball.go
@@ -5,42 +5,42 @@ import (
 )
 
 type Ball struct {
-    PosX, PosY int32
-    DirX, DirY int32
-    Force float64
+	PosX, PosY int32
+	DirX, DirY int32
+	Force      float64
 }
 
 func RandomBall(room Room) Ball {
-    for {
-        ball := Ball {
-            PosX: rand.Int31n(room.Width) + room.PosX,
-            PosY: rand.Int31n(room.Height) + room.PosY,
-            DirX: int32(rand.Intn(3) - 1),
-            DirY: int32(rand.Intn(3) - 1),
-            Force: rand.Float64() * 2 + 1,
-        }
-        if ball.DirX == 0 && ball.DirY == 0 {
-            continue
-        }
-        return ball
-    }
+	for {
+		ball := Ball{
+			PosX:  rand.Int31n(room.Width) + room.PosX,
+			PosY:  rand.Int31n(room.Height) + room.PosY,
+			DirX:  int32(rand.Intn(3) - 1),
+			DirY:  int32(rand.Intn(3) - 1),
+			Force: rand.Float64()*2 + 1,
+		}
+		if ball.DirX == 0 && ball.DirY == 0 {
+			continue
+		}
+		return ball
+	}
 }
 
 func RandomBalls(room Room, n int) []Ball {
-    balls := make([]Ball, n)
-    for i := range n {
-        balls[i] = RandomBall(room)
-    }
-    return balls
+	balls := make([]Ball, n)
+	for i := range n {
+		balls[i] = RandomBall(room)
+	}
+	return balls
 }
 
 func (ball *Ball) NextPosition(room Room) {
-    ball.PosX += int32(float64(ball.DirX) * ball.Force)
-    ball.PosY += int32(float64(ball.DirY) * ball.Force)
-    if ball.PosX < room.PosX || ball.PosX > room.PosX + room.Width {
-        ball.DirX *= -1        
-    }
-    if ball.PosY < room.PosY || ball.PosY > room.PosY + room.Height {
-        ball.DirY *= -1        
-    }
+	ball.PosX += int32(float64(ball.DirX) * ball.Force)
+	ball.PosY += int32(float64(ball.DirY) * ball.Force)
+	if ball.PosX < room.PosX || ball.PosX > room.PosX+room.Width {
+		ball.DirX *= -1
+	}
+	if ball.PosY < room.PosY || ball.PosY > room.PosY+room.Height {
+		ball.DirY *= -1
+	}
 }
